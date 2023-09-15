7fe21d653721dee1d5b0dd2100082cc2f4baf8dd
Alex Vasilianov <ava@scadaminds.com>
2023-09-15 13:51:45 +0200

    Exercises done.

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,7 +1,22 @@
 import Part from './Part'
 
-const Content = ({ part, exercises }) => {
-  return <Part part={part} exercises={exercises} />
+const Content = ({
+  course: {
+    name,
+    parts: [
+      { name: name1, exercises: ex1 },
+      { name: name2, exercises: ex2 },
+      { name: name3, exercises: ex3 },
+    ],
+  },
+}) => {
+  return (
+    <div>
+      <Part part={name1} exercises={ex1} />
+      <Part part={name2} exercises={ex2} />
+      <Part part={name3} exercises={ex3} />
+    </div>
+  )
 }
 
 export default Content
